c2a2fe370da7195aea7cfe467c960a4892c22aab
PurpleNurps <seanlunt1991@gmail.com>
2024-03-07 14:16:58 +0000

    added menu and styled the x to be on top

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -1,50 +1,49 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import styles from "./NavBar.module.css";
 import Image from "next/image";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState({
-    nav_icon1: false,
-    nav_icon2: false,
-    nav_icon3: false,
-    nav_icon4: false,
-  });
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = (id) => {
-    setIsOpen(prevState => ({ ...prevState, [id]: !prevState[id] }));
-  }
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.logo}>
-        <Image src="/logo.png" width={130} height={40} />
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" width={130} height={40} />
+        </div>
+        <div className={styles.burger_container}>
+          <div
+            className={
+              isOpen ? `${styles.nav_icon1} ${styles.open}` : styles.nav_icon1
+            }
+            onClick={() => toggleOpen()}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </nav>
+      <div
+        className={isOpen ? `${styles.menu} ${styles.open}` : styles.closed}
+      >
+        <div className={styles.burger_container}>
+          <div
+            className={
+              isOpen ? `${styles.nav_icon1} ${styles.open}` : styles.nav_icon1
+            }
+            onClick={() => toggleOpen()}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
-      <div className={styles.burger_container}>
-        <div className={isOpen.nav_icon1 ? `${styles.nav_icon1} ${styles.open}` : styles.nav_icon1} onClick={() => toggleOpen('nav_icon1')}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div className={isOpen.nav_icon2 ? `${styles.nav_icon2} ${styles.open}` : styles.nav_icon2} onClick={() => toggleOpen('nav_icon2')}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div className={isOpen.nav_icon3 ? `${styles.nav_icon3} ${styles.open}` : styles.nav_icon3} onClick={() => toggleOpen('nav_icon3')}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div className={isOpen.nav_icon4 ? `${styles.nav_icon4} ${styles.open}` : styles.nav_icon4} onClick={() => toggleOpen('nav_icon4')}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </nav>
+    </>
   );
 }
